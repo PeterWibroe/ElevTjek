429316da96cb9967376112145804b5bba8331bb4
PeterWibroe <58981999+PeterWibroe@users.noreply.github.com>
2020-02-19 21:35:55 +0100

    test

---
+++ protokol.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ElevTjek</title>
+    <meta name="viewport" content="width=device-width, inital-scale=1">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+        <img class="logo" src="images/Elevtjeklogo.png" alt="logo" onclick="window.location.href='index.html'">
+    </header>
+    <nav>
+      <ul class="nav__links">
+        <li><a href="#">Forside</a></li>
+        <li><a href="protokol.php">Protokol</a></li>
+        <li><a href="#">Hjælp</a></li>
+        <li><a href="#">Om projektet</a></li>
+      </ul>
+    </nav>
+  </body>
+</html>
